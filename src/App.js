907f510121576fb5f1ad9623835991b2a5e24637
boahs <39522778+boahs@users.noreply.github.com>
2020-09-01 06:45:20 -0400

    add todo functionality

--- src/App.js
+++ src/App.js
@@ -23,6 +23,17 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  addToDo = (todo) => {
+    const newToDo = {
+      todo: todo,
+      id: Date.now(),
+      completed: false,
+    };
+
+    this.setState({
+      toDoList: [...this.state.toDoList, newToDo],
+    });
+  };
   render() {
     return (
       <div>
